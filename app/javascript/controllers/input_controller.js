b75cd7af40ec36121665386fc8d49c84fbaad94d
DavidAshburn <oneinchwebbing@gmail.com>
2022-09-18 14:53:01 -0700

    input controller working on new_variant view for 9/18 holes

--- app/javascript/controllers/input_controller.js
+++ app/javascript/controllers/input_controller.js
@@ -16,31 +16,30 @@ static targets = [
   connect() {
   }
 
+  //executes when user changes the length on add_variant view
   setLength() {
-    //grab the length and the target div to output our par input rows
+    console.log("setLength");
+    //grab the length and the target divs to output our par input rows
     let length = parseInt(this.lengthselectorTarget.value);
-    let parbox1 = document.querySelector('#parform9');
-    let parbox2 = document.querySelector('#parform18');
-
-    let labels1 = document.querySelector('#labels9');
-    let labels2 = document.querySelector('#labels18');
+    let parbox1 = document.querySelector('#parform1');
+    let parbox2 = document.querySelector('#parform2');
+    let labels1 = document.querySelector('#labels1');
+    let labels2 = document.querySelector('#labels2');
     
-
-    let container1_out = document.createElement("div");
-    container1_out.classList.add("row-center", "col-7");
+    //output containers
+    let parbox1_out = document.createElement("div");
+    let parbox2_out = document.createElement("div");
     let labels1_out = document.createElement("div");
-    container1_out.classList.add("row-center", "col-7");
-
-    let container2_out = document.createElement("div");
-    container2_out.classList.add("row-center", "col-7");
     let labels2_out = document.createElement("div");
-    container1_out.classList.add("row-center", "col-7");
+    parbox1_out.classList.add("row-center", "col-7");
+    parbox2_out.classList.add("row-center", "col-7");
+    labels1_out.classList.add("row-center", "col-7");
+    labels2_out.classList.add("row-center", "col-7");
 
-    
-
+    //generate input fields based on selected length
     for(let i = 1; i <= length * 9; i++) {
       let label_row = document.createElement("div");
-      label_row.classList.add("subtitle");
+      label_row.classList.add("holenumber");
       label_row.innerText = i;
 
       let input_row = document.createElement("input");
@@ -51,25 +50,33 @@ static targets = [
 
       if(i < 10) {
         labels1_out.appendChild(label_row);
-        container1_out.appendChild(input_row);
+        parbox1_out.appendChild(input_row);
       } else {
         labels2_out.appendChild(label_row);
-        container2_out.appendChild(input_row);
+        parbox2_out.appendChild(input_row);
       }
-
     }
 
-    parbox1.removeChild(parbox1.firstElementChild);
-    parbox1.appendChild(container1_out);
+    //making sure they have something before I try to clear it
+    if (parbox1.hasChildNodes()){
+        parbox1.removeChild(parbox1.firstElementChild);
+      }
+    if (labels1.hasChildNodes()){
+      labels1.removeChild(labels1.firstElementChild);
+    }
+    if (parbox2.hasChildNodes()){
+      parbox2.removeChild(parbox2.firstElementChild);
+    }
+    if (labels2.hasChildNodes()){
+      labels2.removeChild(labels2.firstElementChild);
+    }
 
-    labels1.removeChild(labels1.firstElementChild);
+    //output our new divs to HTML
+    parbox1.appendChild(parbox1_out);
     labels1.appendChild(labels1_out);
 
-    if(length > 9) {
-      parbox2.removeChild(parbox2.firstElementChild);
-      parbox2.appendChild(container2_out);
-
-      labels2.removeChild(labels2.firstElementChild);
+    if(length > 1) {
+      parbox2.appendChild(parbox2_out);
       labels2.appendChild(labels2_out);
     }
   }
