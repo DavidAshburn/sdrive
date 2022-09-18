40a55efb64765f57899b23b6c566afe22be6859c
DavidAshburn <oneinchwebbing@gmail.com>
2022-09-18 13:57:22 -0700

    working on new_layout view, 18 holes not working

--- app/javascript/controllers/input_controller.js
+++ app/javascript/controllers/input_controller.js
@@ -3,34 +3,83 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 static targets = [ 
   "lengthselector",
-  "variantparfield"
+  "variantparfield",
+  "par"
   ]
 
   initialize() {
-    this.parinputs = this.variantparfieldTargets;
     this.partotal = 0;
     this.parstring = "";
+    this.setLength();
   }
 
   connect() {
   }
 
   setLength() {
+    //grab the length and the target div to output our par input rows
     let length = parseInt(this.lengthselectorTarget.value);
-    let parbox = this.document.querySelector('#parform');
-    let template = this.document.getElementsByTagName("template")[0];
+    let parbox1 = document.querySelector('#parform9');
+    let parbox2 = document.querySelector('#parform18');
 
-    for(let i = 0; i < length; i++) {
-      parbox.append(template)
-    }
+    let labels1 = document.querySelector('#labels9');
+    let labels2 = document.querySelector('#labels18');
     
+
+    let container1_out = document.createElement("div");
+    container1_out.classList.add("row-center", "col-7");
+    let labels1_out = document.createElement("div");
+    container1_out.classList.add("row-center", "col-7");
+
+    let container2_out = document.createElement("div");
+    container2_out.classList.add("row-center", "col-7");
+    let labels2_out = document.createElement("div");
+    container1_out.classList.add("row-center", "col-7");
+
+    
+
+    for(let i = 1; i <= length * 9; i++) {
+      let label_row = document.createElement("div");
+      label_row.classList.add("subtitle");
+      label_row.innerText = i;
+
+      let input_row = document.createElement("input");
+      input_row.setAttribute("type", "text");
+      input_row.setAttribute("data-input-target", "par");
+      input_row.setAttribute("value", "3");
+      input_row.classList.add("number-select");
+
+      if(i < 10) {
+        labels1_out.appendChild(label_row);
+        container1_out.appendChild(input_row);
+      } else {
+        labels2_out.appendChild(label_row);
+        container2_out.appendChild(input_row);
+      }
+
+    }
+
+    parbox1.removeChild(parbox1.firstElementChild);
+    parbox1.appendChild(container1_out);
+
+    labels1.removeChild(labels1.firstElementChild);
+    labels1.appendChild(labels1_out);
+
+    if(length > 9) {
+      parbox2.removeChild(parbox2.firstElementChild);
+      parbox2.appendChild(container2_out);
+
+      labels2.removeChild(labels2.firstElementChild);
+      labels2.appendChild(labels2_out);
+    }
   }
 
   setPars() {
     this.parstring = "";
-    this.parinputs.forEach(element => {
-      this.parstring.push(parseString(element.innerText));
+    this.parTargets.forEach(element => {
+      this.parstring.push(element.value);
     });
 
+    console.log(this.parstring);
   }
 }
